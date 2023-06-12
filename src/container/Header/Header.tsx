7b5c743327098273cc7e18224e431efbe3e42e33
Lev <levvaschenko@gmail.com>
2023-06-11 18:15:29 -0700

    final fix header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { styled, alpha } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import InputBase from '@mui/material/InputBase'
 import MenuIcon from '@mui/icons-material/Menu'
-import SearchIcon from '@mui/icons-material/Search'
 import Container from '@mui/material/Container'
 import logo from 'assets/logo.png'
 import './Logo.scss'
@@ -16,68 +13,16 @@ import './Container.scss'
 import './Header.scss'
 import UpperSlider from '../Main/Upper-slider'
 import Menu from 'components/Menu'
-import { MenuItem } from '@mui/material'
 
 type Props = {}
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
-}))
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}))
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
-    },
-}))
 
 export default function SearchAppBar(props: Props) {
     return (
         <>
             <Container className="container">
-                <Box sx={{ flexGrow: 1 }} className='box'>
+                <Box sx={{ flexGrow: 1 }} className="box">
                     <AppBar position="static" className="app-bar">
                         <Toolbar>
-                            <IconButton
-                                size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="open drawer"
-                                sx={{ mr: 2 }}
-                            >
-                                <MenuIcon />
-                            </IconButton>
                             <Typography
                                 variant="h6"
                                 noWrap
@@ -95,15 +40,6 @@ export default function SearchAppBar(props: Props) {
                                     ></img>
                                 </a>
                             </Typography>
-                            <Search>
-                                <SearchIconWrapper>
-                                    <SearchIcon />
-                                </SearchIconWrapper>
-                                <StyledInputBase
-                                    placeholder="Search…"
-                                    inputProps={{ 'aria-label': 'search' }}
-                                />
-                            </Search>
                         </Toolbar>
                     </AppBar>
                     <Menu />
