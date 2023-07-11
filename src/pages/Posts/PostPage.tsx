ca2fdd5942ae8c577238bae4e5b24241788ee54a
Lev <levvaschenko@gmail.com>
2023-07-10 22:26:22 -0700

    add page header

--- src/pages/Posts/PostPage.tsx
+++ src/pages/Posts/PostPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import 'container/Array/Posts'
 import { posts } from 'container/Array/Posts';
 import { Box, Container } from '@mui/material';
+import './PostPage.scss'
 
 const PostPage = () => {
     const { postId } = useParams<{ postId?: string }>()
@@ -19,6 +20,26 @@ const PostPage = () => {
 
     return (
         <>
+            <div className="page-header">
+                <div className="page-image">
+                    <Container>
+                        <div className="page-info">
+                            <div className="slider-category">
+                                <Link to={`/${post.category.toLowerCase()}`}>
+                                    {post.category}
+                                </Link>
+                            </div>
+                            <div className="post-title">
+                                {post.title}
+                            </div>
+                            <ul className="page-meta">
+                                <li className="page-date">July 10, 2023</li>
+                                <li className="page-author">Joanna Wellick</li>
+                            </ul>
+                        </div>
+                    </Container>
+                </div>
+            </div>
             <Container>
                 <Box sx={{ flexGrow: 1 }}>
                     <h1>{post.title}</h1>
